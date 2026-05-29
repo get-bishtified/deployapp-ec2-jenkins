1a12568be7ea665d6a05bf0f9cab3ca342c638c1
Shankaz <shankazcloud@gmail.com>
2026-05-29 15:57:57 +0530

    added port 80

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,6 +13,13 @@ resource "aws_security_group" "app_sg" {
   }
 
   ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
     from_port   = 5000
     to_port     = 5000
     protocol    = "tcp"
